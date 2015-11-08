920ca4fcb95bc559db66ff7c70e9d79c38d15f2f
Robert Spencer <robert.spencer94@gmail.com>
2015-11-08 18:40:19 +0200

    Remove skybox

--- Sky.py
+++ Sky.py
@@ -1,11 +1,8 @@
 import numpy as np
 from Shaders import *
-data = np.zeros(8,dtype=[("position" , np.float32,3)])
-data['position'] = [(-1,-1,1),(-1,1,1),(1,-1,1),(1,1,1),
-                    (-1,-1,-1),(-1,1,-1),(1,-1,-1),(1,1,-1),
-                   ]
-data['position'] *= 4000
-I = [0,1,2, 1,2,3, 4,5,6 ,5,6,7, 0,1,4, 1,4,5, 0,2,4, 2,4,6, 3,7,6, 3,6,2, 1,5,7, 1,7,3]
+data = np.zeros(4,dtype=[("position" , np.float32,3)])
+data['position'] = [(-1,-1,0.999999),(-1,1,0.999999),(1,-1,0.999999),(1,1,0.999999)]
+I = [0,1,2, 1,2,3]
 indices = np.array(I,dtype=np.int32)
   
 shader = getShader('sky')
