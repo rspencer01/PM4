be4eb81920eb095f25a97e4e3afd540246b87060
Robert Spencer <robert.spencer94@gmail.com>
2016-04-06 21:26:58 +0200

    Camera has two orientations for space and for on Earth

--- Camera.py
+++ Camera.py
@@ -1,42 +1,55 @@
 import numpy as np
 from transforms import *
 from Shaders import *
+
 class Camera:
   def __init__(self,pos):
-    self.pos = -pos
+    self.pos = pos
+    # Spherical coordinates are used to show the direction we
+    # are looking in
     self.theta = 0
     self.phi = 0 
-    self.update()
+    # The "global" variables are the frame in which we rotate.
+    # This allows, for example, for us to go around the earth
+    # and still have "up" being away from the surface
+    self.globalUp = np.array([0.0,1.0,0.0])
+    self.globalRight = np.array([1.0,0.0,0.0])
+
     self.direction = np.array([0.,0.,1.])
+    self.update()
 
   def move(self,d):
-    self.pos += d*self.direction
     self.update()
+    self.pos -= d*self.direction
 
   def rotUpDown(self,d):
     self.theta += d
-    self.update()
+#    self.update()
 
   def rotLeftRight(self,d):
     self.phi += d
-    self.update()
+#    self.update()
 
   def update(self):
     self.view = np.eye(4,dtype=np.float32)
-    self.view[2,2] = -1
-    translate(self.view,self.pos[0],self.pos[1],self.pos[2])
+    self.view[0:3,0] = self.globalRight[:]
+    self.view[0:3,1] = self.globalUp[:]
+    self.view[0:3,2] = np.cross(self.globalUp,self.globalRight)
+    translate(self.view,-self.pos[0],-self.pos[1],-self.pos[2])
     rotate(self.view,self.phi*180/3.1415,0,1,0)
     rotate(self.view,self.theta*180/3.1415,1,0,0)
-    self.direction = np.array([0.,0.,1.])
+
+    self.direction = np.array([0,0,1])
     ch = np.eye(4,dtype=np.float32)
+    ch[0:3,0] = self.globalRight[:]
+    ch[0:3,1] = self.globalUp[:]
+    ch[0:3,2] = np.cross(self.globalUp,self.globalRight)
     rotate(ch,self.phi*180/3.1415,0,1,0)
     rotate(ch,self.theta*180/3.1415,1,0,0)
     self.direction = ch[:3,:3].dot(self.direction)
 
   def render(self,name=''):
+    self.update()
     setUniform(name+'View',self.view)
-    setUniform(name+'CameraDirection',self.direction*np.array((-1,-1,1)))
-    setUniform(name+'CameraPosition',self.pos*np.array((-1,-1,1)))
-
-
-
+    setUniform(name+'CameraDirection',self.direction)
+    setUniform(name+'CameraPosition',self.pos)
