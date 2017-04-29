a5eed4fb8771f4b2682fa5b6cf43df902b006303
Robert Spencer <robert.spencer94@gmail.com>
2017-04-29 15:22:49 +0200

    Better scale of knight

--- Characters.py
+++ Characters.py
@@ -4,9 +4,9 @@ characters = [
 Object.Object(
   'assets/knight/models/knight.fbx',
   'Knight',
-  scale=0.1,
+  scale=0.03,
   position=(-183, 0,-2938),
-  offset=(-758, -479, -728))]
+  offset=(-758, -477.7, -728))]
 
 
 def display():
