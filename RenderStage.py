1d42d1e5b7a644e930c7f6ffec3a9a66f84cb1ec
Robert Spencer <robert.spencer94@gmail.com>
2017-04-29 15:26:19 +0200

    Clean up RenderStage class and store size

--- RenderStage.py
+++ RenderStage.py
@@ -2,9 +2,12 @@ import OpenGL.GL as gl
 import OpenGL.GL.framebufferobjects as glfbo
 import Texture
 
+
 class RenderStage(object):
   def __init__(self, final_stage=False):
     self.final_stage = final_stage
+    self.width = None
+    self.height = None
     if not final_stage:
       self.create_fbos()
 
@@ -25,15 +28,27 @@ class RenderStage(object):
     else:
       gl.glClear(gl.GL_DEPTH_BUFFER_BIT)
 
+    self.width = width
+    self.height = height
     gl.glViewport(offsetx, offsety, width, height)
 
   def reshape(self, width, height):
-    if self.final_stage: return
+    if self.final_stage:
+      return
     self.displayColorTexture.loadData(width, height, None)
     self.displaySecondaryColorTexture.loadData(width, height, None)
     self.displayAuxColorTexture.loadData(width, height, None)
     self.displayDepthTexture.load()
-    gl.glTexImage2D(gl.GL_TEXTURE_2D, 0,gl.GL_DEPTH_COMPONENT32, width, height, 0,gl.GL_DEPTH_COMPONENT, gl.GL_FLOAT, None)
+    gl.glTexImage2D(
+        gl.GL_TEXTURE_2D,
+        0,
+        gl.GL_DEPTH_COMPONENT32,
+        width,
+        height,
+        0,
+        gl.GL_DEPTH_COMPONENT,
+        gl.GL_FLOAT,
+        None)
 
   def create_fbos(self):
     self.displayFBO = gl.glGenFramebuffers(1)
@@ -46,7 +61,7 @@ class RenderStage(object):
     gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_T, gl.GL_REPEAT)
     gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_NEAREST)
     gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_LINEAR_MIPMAP_LINEAR)
-    self.displayColorTexture.loadData(1,1,None)
+    self.displayColorTexture.loadData(1, 1, None)
 
     self.displaySecondaryColorTexture = Texture.Texture(Texture.COLORMAP2)
     self.displaySecondaryColorTexture.load()
@@ -54,7 +69,7 @@ class RenderStage(object):
     gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_T, gl.GL_REPEAT)
     gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_NEAREST)
     gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_NEAREST_MIPMAP_NEAREST)
-    self.displaySecondaryColorTexture.loadData(1,1,None)
+    self.displaySecondaryColorTexture.loadData(1, 1, None)
 
     self.displayAuxColorTexture = Texture.Texture(Texture.COLORMAP3)
     self.displayAuxColorTexture.load()
@@ -62,7 +77,7 @@ class RenderStage(object):
     gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_T, gl.GL_REPEAT)
     gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_NEAREST)
     gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_NEAREST_MIPMAP_NEAREST)
-    self.displayAuxColorTexture.loadData(1,1,None)
+    self.displayAuxColorTexture.loadData(1, 1, None)
 
     self.displayDepthTexture = Texture.Texture(Texture.DEPTHMAP)
     self.displayDepthTexture.load()
@@ -70,13 +85,13 @@ class RenderStage(object):
     gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_T, gl.GL_REPEAT)
     gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_NEAREST)
     gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_NEAREST)
-    gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_COMPARE_FUNC,gl.GL_LEQUAL)
-    gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_COMPARE_MODE,gl.GL_NONE)
-    gl.glTexImage2D(gl.GL_TEXTURE_2D, 0,gl.GL_DEPTH_COMPONENT32, 1, 1, 0,gl.GL_DEPTH_COMPONENT, gl.GL_FLOAT, None)
+    gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_COMPARE_FUNC, gl.GL_LEQUAL)
+    gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_COMPARE_MODE, gl.GL_NONE)
+    gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_DEPTH_COMPONENT32, 1, 1, 0, gl.GL_DEPTH_COMPONENT, gl.GL_FLOAT, None)
 
     gl.glFramebufferTexture(gl.GL_FRAMEBUFFER, gl.GL_DEPTH_ATTACHMENT,   self.displayDepthTexture.id, 0)
-    gl.glFramebufferTexture(gl.GL_FRAMEBUFFER, gl.GL_COLOR_ATTACHMENT0,  self.displayColorTexture.id, 0);
-    gl.glFramebufferTexture(gl.GL_FRAMEBUFFER, gl.GL_COLOR_ATTACHMENT1,  self.displaySecondaryColorTexture.id, 0);
-    gl.glFramebufferTexture(gl.GL_FRAMEBUFFER, gl.GL_COLOR_ATTACHMENT2,  self.displayAuxColorTexture.id, 0);
+    gl.glFramebufferTexture(gl.GL_FRAMEBUFFER, gl.GL_COLOR_ATTACHMENT0,  self.displayColorTexture.id, 0)
+    gl.glFramebufferTexture(gl.GL_FRAMEBUFFER, gl.GL_COLOR_ATTACHMENT1,  self.displaySecondaryColorTexture.id, 0)
+    gl.glFramebufferTexture(gl.GL_FRAMEBUFFER, gl.GL_COLOR_ATTACHMENT2,  self.displayAuxColorTexture.id, 0)
     gl.glDrawBuffers(3, [gl.GL_COLOR_ATTACHMENT0, gl.GL_COLOR_ATTACHMENT1, gl.GL_COLOR_ATTACHMENT2])
     glfbo.checkFramebufferStatus()
