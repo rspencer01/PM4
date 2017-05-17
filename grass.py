a82ef7d6f96c19bb1536a5c9e3b9970a2c85237c
Robert Spencer <robert.spencer94@gmail.com>
2017-05-17 23:03:18 +0200

    Grass grows up out of ground as you approach

--- grass.py
+++ grass.py
@@ -14,12 +14,24 @@ numberOfPatches = 100
 patchSize = .3
 logging.info(" + {:d} ({:d}x{:d}) patches at {:.3f}m on a side".format((numberOfPatches)**2,numberOfPatches,numberOfPatches,patchSize))
 
-data = np.zeros(numberOfPatches**2,dtype=[("position" , np.float32,3)])
-for i in range(numberOfPatches):
-  for j in range(numberOfPatches):
-    data['position'][i*numberOfPatches+j] = (i,0,j)
-data['position']=(data['position']-np.array([numberOfPatches/2,0,numberOfPatches/2]))*patchSize
-indices = np.array(range(numberOfPatches**2),dtype=np.int32)
+data = np.zeros(numberOfPatches**2,dtype=[("position" , np.float32,3),
+                                          ("occlusion" , np.float32,3)])
+count = 0
+for i in range(-numberOfPatches/2, numberOfPatches/2):
+  for j in range(-numberOfPatches/2, numberOfPatches/2):
+    if i**2 + j**2 < (numberOfPatches/2)**2:
+      data['position'][count] = (i,0,j)
+      data['occlusion'][count] = 0
+      if i**2 + j**2 > (numberOfPatches/3)**2:
+        data['occlusion'][count] = ((i**2 + j**2)**0.5 - numberOfPatches/3) / (numberOfPatches/6)
+
+      count += 1
+data = data[:count]
+logging.info(" + Due to circular, only {} grass patches".format(count))
+
+data['position'] = data['position']*patchSize
+
+indices = np.array(range(count),dtype=np.int32)
 
 shader = getShader('grass3',geom=True,forceReload=True)
 Terrain.setTerrainUniforms(shader)
