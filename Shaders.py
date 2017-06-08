74842e311e914704ca36ee0958fd94aa5dac87f9
Robert Spencer <robert.spencer94@gmail.com>
2017-06-08 15:22:02 +0200

    Cleanup shader code and introduce tranform feedback shaders

--- Shaders.py
+++ Shaders.py
@@ -4,21 +4,20 @@ import numpy as np
 import OpenGL.GL as gl
 from ctypes import c_void_p, sizeof
 import re
+from collections import namedtuple
 
 currentShader = None
 universalUniforms = {}
 
-class objectInfo:
-  def __init__(self,nv,ni,vbo,ver,ibo,renderVerts,instbo=None):
-    self.numVertices = nv
-    self.numIndices = ni
-    self.vbo = vbo
-    self.vertexArray = ver
-    self.ibo = ibo
-    self.instbo = instbo
-    self.renderVerts = renderVerts
-  def __repr__(self):
-    return "<objectInfo (v/i {:d}/{:d} | vbo/ibo/ver/instbo {:d}/{:d}/{:d}/{:d}>".format(self.numVertices,self.numIndices,self.vbo,self.ibo,self.vertexArray,self.instbo)
+objectInfo = namedtuple("ObjectInfo",
+    ("numVertices",
+      "numIndices",
+      "vbo",
+      "vertexArray",
+      "ibo",
+      "renderVerts",
+      "instbo",
+      "transformBufferObject"))
 
 class ShaderCompileException(Exception):
   def __init__(self, args):
@@ -36,7 +35,7 @@ class ShaderCompileException(Exception):
     Exception.__init__(self, ret)
 
 class Shader(object):
-  def __init__(self,name):
+  def __init__(self, name):
     # Who are we
     self.name = name
     # What do we stand for?
@@ -51,13 +50,16 @@ class Shader(object):
     # Uniforms that are still to be set
     self.unsetUniforms = {}
 
-  def addProgram(self,type,source):
+  def addProgram(self, type, source):
+    """Creates a shader, compiles the given shader source and attaches it
+    to this program."""
     prog = gl.glCreateShader(type)
-    gl.glShaderSource(prog,source)
+    gl.glShaderSource(prog, source)
     gl.glCompileShader(prog)
+    # Find compile errors
     if gl.glGetShaderiv(prog, gl.GL_COMPILE_STATUS) != gl.GL_TRUE:
         raise ShaderCompileException((gl.glGetShaderInfoLog(prog), source))
-    gl.glAttachShader(self.program,prog)
+    gl.glAttachShader(self.program, prog)
 
   def build(self):
     # Link!  Everything else is done in addProgram
@@ -65,7 +67,7 @@ class Shader(object):
     if gl.glGetProgramiv(self.program, gl.GL_LINK_STATUS) != gl.GL_TRUE:
       raise RuntimeError(gl.glGetProgramInfoLog(self.program))
     for prog in self.programs:
-      gl.glDetachShader(self.program,prog)
+      gl.glDetachShader(self.program, prog)
 
   def load(self):
     # Check if we are loaded already.  If not do so.
@@ -74,7 +76,7 @@ class Shader(object):
       gl.glUseProgram(self.program)
       currentShader = self
 
-  def setData(self,data,indices,instanced=False):
+  def setData(self,data, indices=[], instanced=False):
     """Given vertex data and triangle indices, gives a unique identifier to be
     used when rendering this object."""
     vbo = gl.glGenBuffers(1)
@@ -86,28 +88,30 @@ class Shader(object):
                                     vbo,
                                     vertexArray,
                                     ibo,
-                                    len(indices)*3
+                                    len(indices)*3,
+                                    None, None
                                     ) )
 
-    gl.glBindBuffer(gl.GL_ARRAY_BUFFER,vbo)
-    gl.glBufferData(gl.GL_ARRAY_BUFFER,data.nbytes,data,gl.GL_STATIC_DRAW)
+    gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vbo)
+    gl.glBufferData(gl.GL_ARRAY_BUFFER, data.nbytes, data, gl.GL_STATIC_DRAW)
     stride = data.strides[0]
     offsetc = 0
     for i in data.dtype.names:
       offset = ctypes.c_void_p(offsetc)
-      loc = gl.glGetAttribLocation(self.program,i)
+      loc = gl.glGetAttribLocation(self.program, i)
       if loc==-1:
         # Not in shader
         offsetc += data.dtype[i].itemsize
         continue
       gl.glEnableVertexAttribArray(loc)
       gl.glBindBuffer(gl.GL_ARRAY_BUFFER,vbo)
-      gl.glVertexAttribPointer(loc,np.prod(data.dtype[i].shape),gl.GL_FLOAT,False,stride,offset)
-      gl.glVertexAttribDivisor(loc,0)
+      gl.glVertexAttribPointer(loc, int(np.prod(data.dtype[i].shape)), gl.GL_FLOAT, False, stride, offset)
+      gl.glVertexAttribDivisor(loc, 0)
       offsetc += data.dtype[i].itemsize
 
-    gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER,ibo)
-    gl.glBufferData(gl.GL_ELEMENT_ARRAY_BUFFER,indices.nbytes,indices, gl.GL_STREAM_DRAW);
+    if indices != []:
+      gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, ibo)
+      gl.glBufferData(gl.GL_ELEMENT_ARRAY_BUFFER, indices.nbytes, indices, gl.GL_STREAM_DRAW);
 
     return len(self.objInfo)-1
 
@@ -117,40 +121,34 @@ class Shader(object):
     gl.glDeleteVertexArrays(obj.vertexArray)
 
   def __setitem__(self,i,v):
-    """
-    Sets the uniform lazily.  That is, we store the uniform CPU side, and only blitz it once
-    we are about to render.  See `_setitem`.
-    """
+    """Sets the uniform lazily.  That is, we store the uniform CPU side, and 
+    only blit it once we are about to render.  See `_setitem`."""
     self.unsetUniforms[i] = v
 
   def _setitem(self,i,v):
-    """
-    Actually sets the uniform in the shader.  Due to GPU overhead, this is not called via
-    the [] notation, but only when we render.
-    """
+    """Sets the uniform in the shader.  Due to GPU overhead, this is not called 
+    via the [] notation, but only when we render."""
     if i not in self.locations:
-      self.locations[i] = gl.glGetUniformLocation(self.program,i)
+      self.locations[i] = gl.glGetUniformLocation(self.program, i)
     loc = self.locations[i]
     if loc==-1:
       if not i in self.warned:
         self.warned.add(i)
-    elif type(v) == np.ndarray and v.shape == (4,4):
-      gl.glUniformMatrix4fv(loc,1,gl.GL_FALSE,v)
+    elif type(v) == np.ndarray and v.shape == (4, 4):
+      gl.glUniformMatrix4fv(loc, 1, gl.GL_FALSE, v)
     elif type(v) == np.ndarray and len(v.shape)==2 and v.shape[1]==3:
-      gl.glUniform3fv(loc,v.shape[0],v)
+      gl.glUniform3fv(loc, v.shape[0], v)
     elif type(v) == np.ndarray and len(v.shape)==2 and v.shape[1]==4:
-      gl.glUniform4fv(loc,v.shape[0],v)
-    elif type(v) in [float,np.float32,np.float64]:
+      gl.glUniform4fv(loc, v.shape[0], v)
+    elif type(v) in [float, np.float32, np.float64]:
       gl.glUniform1f(loc,v)
     elif type(v) in [np.ndarray] and v.shape==(3,):
-      gl.glUniform3f(loc,v[0],v[1],v[2])
+      gl.glUniform3f(loc, v[0], v[1], v[2])
     elif type(v) in [int]:
-      gl.glUniform1i(loc,v)
+      gl.glUniform1i(loc, v)
 
   def _setitems(self):
-    """
-    Blits all the lazyloaded uniforms to the GPU.
-    """
+    """Blits all the lazyloaded uniforms to the GPU.    """
     for i,v in self.unsetUniforms.items():
       self._setitem(i,v)
     self.unsetUniforms = {}
@@ -162,12 +160,12 @@ class Shader(object):
     gl.glDrawElements(type,self.objInfo[objectIndex].numIndices,gl.GL_UNSIGNED_INT,None)
 
 class GenericShader(Shader):
-  def __init__(self,name,frag,vert,geom):
+  def __init__(self, name, frag, vert, geom):
     super(GenericShader,self).__init__(name)
-    self.addProgram(gl.GL_VERTEX_SHADER,vert)
+    self.addProgram(gl.GL_VERTEX_SHADER, vert)
     if geom:
-      self.addProgram(gl.GL_GEOMETRY_SHADER,geom)
-    self.addProgram(gl.GL_FRAGMENT_SHADER,frag)
+      self.addProgram(gl.GL_GEOMETRY_SHADER, geom)
+    self.addProgram(gl.GL_FRAGMENT_SHADER, frag)
     self.build()
 
 class InstancedShader(GenericShader):
@@ -183,26 +181,26 @@ class InstancedShader(GenericShader):
   def setInstances(self,instances,renderId):
     gl.glBindVertexArray(self.objInfo[renderId].vertexArray)
     self.instbo = gl.glGenBuffers(1)
-    self.objInfo[renderId].instbo = self.instbo
+    self.objInfo[renderId] = self.objInfo[renderId]._replace(instbo=self.instbo)
 
-    gl.glBindBuffer(gl.GL_ARRAY_BUFFER,self.instbo)
-    gl.glBufferData(gl.GL_ARRAY_BUFFER,instances.nbytes,instances,gl.GL_STREAM_DRAW)
+    gl.glBindBuffer(gl.GL_ARRAY_BUFFER, self.instbo)
+    gl.glBufferData(gl.GL_ARRAY_BUFFER, instances.nbytes, instances, gl.GL_STREAM_DRAW)
     stride = instances.strides[0]
     offsetc = 0
     for i in instances.dtype.names:
       offset = ctypes.c_void_p(offsetc)
-      loc = gl.glGetAttribLocation(self.program,i)
+      loc = gl.glGetAttribLocation(self.program, i)
       if loc==-1:
         print "Error setting "+i+" in shader "+self.name
         continue
-      gl.glBindBuffer(gl.GL_ARRAY_BUFFER,self.instbo)
-      if instances.dtype[i].shape==(4,4):
+      gl.glBindBuffer(gl.GL_ARRAY_BUFFER, self.instbo)
+      if instances.dtype[i].shape == (4, 4):
         for j in range(4):
           offset = ctypes.c_void_p(j*stride/4)
           gl.glEnableVertexAttribArray(loc+j)
-          gl.glVertexAttribPointer(loc+j,4,gl.GL_FLOAT,False,stride,offset)
-          gl.glVertexAttribDivisor(loc+j,1)
-          offsetc+=4*2
+          gl.glVertexAttribPointer(loc+j, 4, gl.GL_FLOAT, False, stride, offset)
+          gl.glVertexAttribDivisor(loc+j, 1)
+          offsetc += 4*2
       else:
         raise Exception("Type wrong for instanced variable")
     gl.glBindBuffer(gl.GL_ARRAY_BUFFER,0)
@@ -221,23 +219,61 @@ class InstancedShader(GenericShader):
                                )
 
 class TesselationShader(Shader):
-  def __init__(self,name,frag,vert,geom,tessC,tessE):
+  def __init__(self, name, frag, vert, geom, tessC, tessE):
     super(TesselationShader, self).__init__(name)
-    self.addProgram(gl.GL_VERTEX_SHADER,vert)
+    self.addProgram(gl.GL_VERTEX_SHADER, vert)
     if geom:
-      self.addProgram(gl.GL_GEOMETRY_SHADER,geom)
-    self.addProgram(gl.GL_FRAGMENT_SHADER,frag)
-    self.addProgram(gl.GL_TESS_CONTROL_SHADER,tessC)
-    self.addProgram(gl.GL_TESS_EVALUATION_SHADER,tessE)
+      self.addProgram(gl.GL_GEOMETRY_SHADER, geom)
+    self.addProgram(gl.GL_FRAGMENT_SHADER, frag)
+    self.addProgram(gl.GL_TESS_CONTROL_SHADER, tessC)
+    self.addProgram(gl.GL_TESS_EVALUATION_SHADER, tessE)
     self.build()
 
-  def draw(self,number,objectIndex):
+  def draw(self, number, objectIndex):
     self.load()
     self._setitems()
     gl.glBindVertexArray(self.objInfo[objectIndex].vertexArray)
-    gl.glPatchParameteri(gl.GL_PATCH_VERTICES,3)
-    gl.glDrawArrays(gl.GL_PATCHES,0,number)
+    gl.glPatchParameteri(gl.GL_PATCH_VERTICES, 3)
+    gl.glDrawArrays(gl.GL_PATCHES, 0, number)
 
+class TransformFeedbackShader(Shader):
+  def addOutput(self, name):
+    """Registers an output of the transform shader."""
+    varyings = ctypes.c_char_p(name)
+    varyings_pp = ctypes.cast(ctypes.pointer(varyings), ctypes.POINTER(ctypes.POINTER(gl.GLchar)))
+    gl.glTransformFeedbackVaryings(self.program, 1, varyings_pp, gl.GL_INTERLEAVED_ATTRIBS)
+
+  def getOutputBufferObject(self, objectIndex, max_size):
+    """Gets an output buffer for the given input object.
+
+    TODO Not sure about this here.  How should we be doing this better?"""
+    tbo = gl.glGenBuffers(1)
+
+    gl.glBindVertexArray(self.objInfo[objectIndex].vertexArray)
+    gl.glBindBuffer(gl.GL_TRANSFORM_FEEDBACK_BUFFER, tbo)
+    gl.glBufferData(gl.GL_TRANSFORM_FEEDBACK_BUFFER, max_size, None, gl.GL_STATIC_READ)
+    gl.glBindBufferBase(gl.GL_TRANSFORM_FEEDBACK_BUFFER, 0, tbo)
+
+    return tbo
+
+  def draw(self, type, objectIndex, count=0):
+    """Starts a transform feedback draw.  Return the number of items actually
+    created (may differ from `num` due to geometry shaders)."""
+    gl.glBindVertexArray(self.objInfo[objectIndex].vertexArray)
+    gl.glEnable(gl.GL_RASTERIZER_DISCARD)
+    query = gl.glGenQueries(1)
+    gl.glBeginQuery(gl.GL_TRANSFORM_FEEDBACK_PRIMITIVES_WRITTEN, query)
+    gl.glBeginTransformFeedback(type)
+    gl.glDrawArrays(gl.GL_POINTS, 0, count)
+    gl.glEndTransformFeedback()
+    gl.glEndQuery(gl.GL_TRANSFORM_FEEDBACK_PRIMITIVES_WRITTEN)
+    gl.glDisable(gl.GL_RASTERIZER_DISCARD)
+    gl.glFlush()
+    count = gl.glGetQueryObjectiv(query, gl.GL_QUERY_RESULT)
+    gl.glDeleteQueries(1, [query])
+    return count
+
+gl.glFlush()
 
 shaders = {}
 
@@ -260,7 +296,7 @@ def readShaderFile(filename):
     m = p.search(source)
   return source
 
-def getShader(name,tess=False,instance=False,geom=False,forceReload=True):
+def getShader(name, tess=False, instance=False, geom=False, forceReload=True):
   global shaders
   if name not in shaders or forceReload:
     logging.info("Loading shader '{:s}'".format(name))
