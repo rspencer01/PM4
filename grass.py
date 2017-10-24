0c46c8610eed28e4c943f4fd4131e62e8393f4be
Robert Spencer <general@robertandrewspencer.com>
2017-10-24 21:39:39 +0100

    Tweak grass

--- grass.py
+++ grass.py
@@ -2,15 +2,20 @@ import logging
 import numpy as np
 import dent.Texture as Texture
 from dent.Shaders import *
+from dent.configuration import config
 
 logging.info("Loading grass")
 
 texture = Texture.Texture(Texture.COLORMAP)
 texture.loadFromImage('assets/grass.png')
 
-numberOfPatches = 100
+numberOfPatches = int(100 * config.grass_amount)
 patchSize = .3
-logging.info(" + {:d} ({:d}x{:d}) patches at {:.3f}m on a side".format((numberOfPatches)**2,numberOfPatches,numberOfPatches,patchSize))
+logging.info(" {:d} ({:d}x{:d}) patches at {:.3f}m on a side".format(
+  (numberOfPatches)**2,
+  numberOfPatches,
+  numberOfPatches,
+  patchSize))
 
 data = np.zeros(numberOfPatches**2,dtype=[("position" , np.float32,3),
                                           ("occlusion" , np.float32,3)])
@@ -18,28 +23,28 @@ count = 0
 for i in range(-numberOfPatches/2, numberOfPatches/2):
   for j in range(-numberOfPatches/2, numberOfPatches/2):
     if i**2 + j**2 < (numberOfPatches/2)**2:
-      data['position'][count] = (i,0,j)
-      data['occlusion'][count] = 0
+      data['position'][count] = (i * patchSize, 0, j * patchSize)
       if i**2 + j**2 > (numberOfPatches/3)**2:
         data['occlusion'][count] = ((i**2 + j**2)**0.5 - numberOfPatches/3) / (numberOfPatches/6)
-
+      else:
+        data['occlusion'][count] = 0
       count += 1
+
 data = data[:count]
-logging.info(" + Due to circular, only {} grass patches".format(count))
+logging.info(" Due to circular, only {} grass patches".format(count))
 
-data['position'] = data['position']*patchSize
+indices = np.array(range(count), dtype=np.int32)
 
-indices = np.array(range(count),dtype=np.int32)
-
-shader = getShader('grass3',geom=True)
+shader = getShader('grass3', geom=True)
 shader['patchSize'] = patchSize
 shader['colormap'] = Texture.COLORMAP_NUM
-shader['noisemap'] = Texture.NOISE_NUM
 
 renderID = shader.setData(data,indices)
 
 def display(cam):
-  if np.sum(cam.position*cam.position) > 6e4**2 or cam.position[1]>4e3:
+  if abs(cam.position[0]) > config.world_size or \
+     abs(cam.position[2]) > config.world_size or \
+     cam.position[1]>4e3:
     return
   texture.load()
   shader.load()
